593b232f0942ae8ad13be6c5516657a04e624027
seejn <seejnmaharjan@gmail.com>
2024-03-27 17:20:47 +0545

    installation: mongoose
    configuration for mongoDB connection
    - config.js: mongoDBURL = "mongodb/url"

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,58 @@
-const serverMsg = "Server Listening";
-console.log(serverMsg);
+import express from "express";
+import mongoose from "mongoose";
+// import { PORT, mongoDBURL } from "./config.js";
+import { PORT } from "./config.js";
+
+const app = express();
+const serverMsg = `Server Listening on ${PORT}`;
+
+// middleware to parse request body
+app.use(express.json());
+
+/*
+	// configuration to connect to mongoDB
+
+	mongoose
+		.connect(mongoDBURL)
+		.then(() => {
+			console.log("App connected to DB");
+			app.listen(PORT, () => {
+				console.log(serverMsg);
+			});
+		})
+		.catch((error) => {
+			return next(error);
+		});
+*/
+
+// server initialization
+
+app.listen(PORT, () => {
+	console.log(serverMsg);
+});
+
+// root route
+
+app.get('/', (req, res, next) => {
+	const responseData = {
+		"message": serverMsg
+	}
+	return res.status(200).json(responseData);
+});
+
+app.use((req, res, next) => {
+	const error = new Error("Route not Found!!!");
+	error.status = 404;
+	return next(error);
+});
+
+app.use((error, req, res, next) => {
+	const status = error.status || 500;
+	const responseData = {
+		"error": {
+			"message": error.message || "Server Error",
+			"status": status
+		}
+	}
+	return res.status(status).json(responseData);
+});
